a5e67c57d02ba27a2c1af56056dc6b905c12fbfb
Aimee-MacDonald <admin@aimeemacdonald.com>
2017-09-21 01:31:29 +0200

    Installed pug and Configured the Server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.set("view engine", "pug");
+
 app.get("/", function(req, res){
   res.send("<h1>Meep!</h1>");
 });
