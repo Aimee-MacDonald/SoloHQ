36b8085f99e74dc9c88ebc0b801be035e9179971
Aimee-MacDonald <admin@aimeemacdonald.com>
2017-09-21 01:25:00 +0200

    Started Server on Port 8080

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("Meep!");
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.listen(port, function(){
+  console.log("Server Listening on Port: " + port);
+});
