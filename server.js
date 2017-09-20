306e0b6cdf77d0a20bf2663a8f61e67e5a42b395
Aimee-MacDonald <admin@aimeemacdonald.com>
2017-09-21 01:28:54 +0200

    Plugged Root Route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.get("/", function(req, res){
+  res.send("<h1>Meep!</h1>");
+});
+
 app.listen(port, function(){
   console.log("Server Listening on Port: " + port);
 });
