706eb2b7d8af8f3410d951264158ec9fb961df03
Aimee-MacDonald <admin@aimeemacdonald.com>
2017-09-21 01:58:03 +0200

    Created and Served a Basic pug Page with css and js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(express.static(path.join(__dirname, "static")));
+
 app.get("/", function(req, res){
-  res.send("<h1>Meep!</h1>");
+  res.render("home");
 });
 
 app.listen(port, function(){
