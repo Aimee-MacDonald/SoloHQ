adf70800834100627697efd885dd8ebba7a83b67
Aimee-MacDonald <admin@aimeemacdonald.com>
2017-09-22 17:51:00 +0200

    Added a basic login form

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,1 +1,25 @@
-console.log("Moop!");
+
+
+
+
+
+function btnLogin(){
+  var lf = new LoginForm;
+  var el_Head = document.getElementById("header");
+  el_Head.innerHTML = lf.render() + el_Head.innerHTML;
+}
+
+class LoginForm{
+  render(){
+    return `
+      <div id="loginform">
+        <form>
+          <button>Register</button>
+          <input type="text" placeholder="Email">
+          <input type="password" placeholder="Password">
+          <button>Login</button>
+        </form>
+      </div>
+    `
+  }
+}
