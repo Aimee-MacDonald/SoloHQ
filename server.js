a09712f0cef14e7c9c21e1572456ae51b1130d15
Aimee-MacDonald <admin@aimeemacdonald.com>
2017-09-23 02:41:45 +0200

    Installed Mongo and Connected to a Database

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const app = express();
 const port = 8080;
 const path = require("path");
 require("dotenv").config();
+const mongo = require("mongodb").MongoClient;
+
+var database;
+
+console.log("Connecting..");
+mongo.connect(process.env.DBURL, function(err, db){
+  if(err) throw err;
+
+  database = db;
+  app.listen(port, function(){
+    console.log("Server Listening on Port: " + port);
+  });
+});
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -11,9 +24,4 @@ app.use(express.static(path.join(__dirname, "static")));
 
 app.get("/", function(req, res){
   res.render("dashboard");
-});
-
-app.listen(port, function(){
-  console.log("Server Listening on Port: " + port);
-  console.log(process.env.NAME);
 });
